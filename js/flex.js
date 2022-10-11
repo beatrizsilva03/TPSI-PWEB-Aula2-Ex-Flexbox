2776f588b8f77e4dd5855a0f8dc2e972567969db
Beatiz Silva <biselva2000@gmail.com>
2022-10-11 17:29:12 +0100

    estrutura nova

--- js/flex.js
+++ js/flex.js
@@ -4,3 +4,24 @@ function flex(valor){
 function wrap(valor){
     document.getElementById('container').style.flexWrap=valor;
 }
+function justify(valor){
+    document.getElementById('container').style.justifyContent=valor;
+}
+function align(valor){
+    document.getElementById('container').style.alignItems=valor;
+}
+function slide(){
+    document.getElementById('publicidade').src="../img/email.jpg";
+    document.getElementById('descricao').innerHTML="email";
+    setTimeout("slide2()",2000)
+}
+function slide2(){
+    document.getElementById('publicidade').src="../img/imc.jpg";
+    document.getElementById('descricao').innerHTML="imc";
+    setTimeout("slide3()",2000)
+}
+function slide3(){
+    document.getElementById('publicidade').src="../img/logo.jpg";
+    document.getElementById('descricao').innerHTML="isla";
+    setTimeout("slide()",2000)
+}
